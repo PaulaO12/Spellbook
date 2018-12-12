5b8569582ba7aa9d304f9bbcf381b0422ac72f07
PaulaO12 <44788452+PaulaO12@users.noreply.github.com>
2018-12-12 17:52:56 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,11 @@
+$(".onespell").click(function(){
+    $("#Spell1").slideToggle();
+});
+
+$(".twospell").click(function(){
+    $("#spellTwo").fadeToggle();
+});
+
+$(".threespell").click(function(){
+    $("#spellThree").toggle();
+});
